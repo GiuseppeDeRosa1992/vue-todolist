025bd7d27c290ea032018c8cb2f6c230c3810a55
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-14 15:04:25 +0200

    m-0 added array in vue.js and added tag h1 and ul with li with cycle v-for that print element array inside page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,23 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+        title : "La tua lista comprende:",
+        array : [
+          "Pane",
+          "Acqua",
+          "Sale",
+          "Pasta",
+          "Pomodoro",
+          "Paella",
+          "Pan Gocciole",
+          "Cioccolata",
+          "Farina",
+          "Pizza",
+        ]
         
       }
+    },
+    methods: {
+
     }
   }).mount('#app')
